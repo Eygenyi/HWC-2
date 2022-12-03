325516bee89a45d0aa0cb48aa2a72815d7a02fd5
Evgeniy <freemen0083@gmail.com>
2022-12-03 12:56:33 +0400

    #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("input number 100 - 1000");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DelHunOne()
+{
+    int ten = num / 10 % 10;
+
+    return (ten);
+}
+  if (num >999 || num <100 || num<0)
+  {
+    Console.WriteLine("error");
+  }
+else
+{
+int res = DelHunOne();
+Console.WriteLine(res);
+}
