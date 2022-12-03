0ccc2f4ee83f21df352df6bb5649983a87d4c0f5
Evgeniy <freemen0083@gmail.com>
2022-12-03 21:14:34 +0400

    #3

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("input day number");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        void DayOfWeek()
+        {
+
+            if (day ==7 || day == 6)
+                Console.WriteLine("EEEE day off");
+            else
+            {
+                Console.WriteLine("Working day");
+            }
+        }
+if (day >7 || day <1 || day==0)
+  {
+    Console.WriteLine("error");
+  }
+else
+{
+  
+DayOfWeek ();
+}
+  
+
