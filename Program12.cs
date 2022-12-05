c660d3f58625c2bb8a9c35d0fd9316085deb6ac5
Evgeniy <freemen0083@gmail.com>
2022-12-05 22:06:18 +0400

    #2

---
+++ Program12.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("input any number");
+int num = Convert.ToInt32(Console.ReadLine());
+int Third(int find=0)
+{
+    while (num > 100)
+    {find = num % 10; 
+    num = num /10;}
+    return (find);
+}
+
+if (num<100)
+Console.WriteLine("No third digit");
+else 
+{
+int r=Third();
+Console.WriteLine(r);
+}
